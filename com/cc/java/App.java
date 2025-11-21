9cda8bcfe52d5f9a025a9527f0fe2084bc0a8861
Peter <schmidt.multimedia@gmail.com>
2025-11-21 11:17:41 +0100

    03 / static output() + return

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -4,21 +4,37 @@ public class App {
     
     public static void main(String[] args) {
 
-        // System.out.println(new Cat());
-
-        // Instanziierung
-        // cat = Referenzvariable
-        // Cat = Referenztyp
+        /*
+       *    2. Iteration
+       *    Instanziierung  --> 
+       *    Erzeugen eines Objektes durch Aufruf eines Konstruktors
+       *    Objekt <---> Instanz
+       *    cat --> Referenzvariable
+       *    Cat --> Referenztyp(Klasse)
+       * 
+       */
 
         Cat cat = new Cat();
-        System.out.println("Blick von aussen: " + cat);
+        // IMPLIZITE Typumwandlung  Cat --> String 
+        output("Blick von aussen: " + cat);
+        output("Blick von innen: " + cat.getInstanceVariable());
+         //cat.tellYourAddress();
+        output("--------------------");
+       
+        Cat cat2 = new Cat();
+        output("Blick von aussen: " + cat2);
+        output("Blick von innen: " + cat2.getInstanceVariable());
+        output("--------------------");
 
-        // cat.sayHello();
-        cat.tellYourAddress();
 
     }
 
-   
+
+    // statische Methode
+    public static void output(String outputStr){
+        System.out.println(outputStr);
+    }
+     
 
 }
 
--- com/cc/java/Cat.java
+++ com/cc/java/Cat.java
@@ -3,15 +3,15 @@ package com.cc.java;
 public class Cat {
 
 
-    public void sayHello(){
-        System.out.println("Hello!");
-    }
-
     // this --> Instanzvariable
     public void tellYourAddress(){
-        System.out.println("Blick von innen: " + this);
+        App.output("Blick von innen: " + this);
     }
 
-    
+    public Cat getInstanceVariable(){
+        return this;
+    }
+
+
     
 }
